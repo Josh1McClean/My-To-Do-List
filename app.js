de018209bc37e6e26910683ebf2716b05eb8492a
Josh McClean <joshuamcclean1@gmail.com>
2024-04-14 23:30:09 -0400

    Fourth Edit

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 function addToDo() {
+    //section1:
     let todoElement = document.getElementById("new-todo");
     let todo = todoElement.value;
     todoElement.value = "";
 
+    //section2:
     if (todo.length < 3) {
         return alert("At Least 3 Letters To Add Tasks For Todo List");
     }
+
+    //section3:
     let list = document.getElementById("list");
     let li = document.createElement("li");
     let text = document.createTextNode(todo);
+
+    //section4:
+    li.setAttribute("onclick", "this.remove()")
     li.appendChild(text);
     list.appendChild(li);
 }
